77149ef73bc69c3316f6cc0389b5ec6709170573
Chris Bradel <ChrisBradel14@gmail.com>
2021-08-19 23:31:31 -0400

    playable robo

---
+++ Assets/CarMovementScript.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CarMovementScript : MonoBehaviour
+{
+    private PlayerControls playerControls;
+
+    public float MotorSpeed;
+    public WheelCollider FL;
+    public WheelCollider FR;
+    public WheelCollider BL;
+    public WheelCollider BR;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    int sign(float input)
+    {
+        if (input < 0)
+            return -1;
+        else if (input == 0)
+            return 0;
+        else
+            return 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = sign(playerControls.Player.Move.ReadValue<Vector2>().x);
+        float v = sign(playerControls.Player.Move.ReadValue<Vector2>().y);
+
+        if (h != 0)
+        {
+            FL.motorTorque = h * MotorSpeed * 1;
+            FR.motorTorque = h * MotorSpeed * -1;
+            BL.motorTorque = h * MotorSpeed * 1;
+            BR.motorTorque = h * MotorSpeed * -1;
+        }
+        else
+        {
+            if (v != 0)
+            {
+                FL.motorTorque = v * MotorSpeed;
+                FR.motorTorque = v * MotorSpeed;
+                BL.motorTorque = v * MotorSpeed;
+                BR.motorTorque = v * MotorSpeed;
+            }
+        }
+
+        if (h == 0 && v == 0)
+        {
+            FL.motorTorque = 0;
+            FR.motorTorque = 0;
+            BL.motorTorque = 0;
+            BR.motorTorque = 0;
+        }
+
+        Debug.Log(h.ToString() + ", " + v.ToString());
+    }
+}
---
+++ Assets/CozmoBotGeneral.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CozmoBotGeneral : MonoBehaviour
+{
+    public bool debug_mode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        debug_mode = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
